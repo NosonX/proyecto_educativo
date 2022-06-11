eb7eecbeb189bdcfc998a92f5def7f531aac3e8f
Eduardo Alfaro <eam2092@gmail.com>
2022-06-10 22:13:01 -0600

    expandable container and increase, decrease buttons

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,36 @@
 window.onload = () => {
+    // Increase and Decrease buttons
     const decreaseButtons = document.querySelectorAll('button.decrease');
     const increaseButtons = document.querySelectorAll('button.increase');
 
-    // console.log(decreaseButtons);
-    // console.log(increaseButtons);
-
-    const changeInputNumber = (event, increase = false) => {
+    const changeInputValue = (event, amount) => {
         const input = event.target.parentNode.querySelector('input');
-        const inputValue = parseInt(input.value);
-        input.value = increase ? inputValue + 1 : inputValue - 1;
-    }
+        const value = parseInt(input.value);
+        input.value = value + amount;
+    };
 
-    decreaseButtons.forEach(button => {
-        button.addEventListener('click', (event) => changeInputNumber(event));
+    decreaseButtons.forEach((button) => {
+        button.addEventListener('click', (event) => changeInputValue(event, -1));
     })
 
-    increaseButtons.forEach(button => {
-        button.addEventListener('click', (event) => changeInputNumber(event, true));
-    })
-}
+    increaseButtons.forEach((button) => {
+        button.addEventListener('click', (event) => changeInputValue(event, 1));
+    });
+
+
+
+    // Collapsable panel
+    const sectionHeaders = document.querySelectorAll('.section-header');
+
+    sectionHeaders.forEach(sectionHeader => {
+        sectionHeader.addEventListener('click', (event) => {
+            const excerciseContainer = event.target.parentNode.querySelector('.excersices-container');
+            
+            if (excerciseContainer.classList.contains('close')) {
+                excerciseContainer.classList.remove('close');
+            } else {
+                excerciseContainer.classList.add('close');
+            }
+        });
+    });
+} 
