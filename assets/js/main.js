34b78145a6f11a2a0a429cc45e89babde7160ac6
dannier29 <55514134+dannier29@users.noreply.github.com>
2022-06-13 18:00:47 -0600

    Iconos listos

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,12 +23,172 @@ window.onload = () => {
     sectionHeaders.forEach(sectionHeader => {
         sectionHeader.addEventListener('click', (event) => {
             const excerciseContainer = event.target.parentNode.querySelector('.excersices-container');
-
+            const iconos = event.target.parentNode.querySelector('.iconos');
             if (excerciseContainer.classList.contains('close')) {
                 excerciseContainer.classList.remove('close');
+                iconos.classList.remove('close');
             } else {
                 excerciseContainer.classList.add('close');
+                iconos.classList.add('close');
             }
         });
     });
+
+    // Generate Test
+    let test = {
+        sumas: [],
+        restas: [],
+        multiplicaciones: [],
+        divisiones: []
+    };
+
+    const getRandomNumber = (min, max) => Math.floor(Math.random() * (max + 1 - min)) + min;
+
+    const getHtmlExcercise = (index, testKey) => {
+        const min = parseInt(document.querySelector('input[name="min"]').value);
+        const max = parseInt(document.querySelector('input[name="max"]').value);
+        const number1 = getRandomNumber(min, max);
+        const number2 = getRandomNumber(min, max);
+        const valueToComplete = getRandomNumber(0, 2);
+        const numbers = [
+            number1,
+            number2,
+            testKey == 'sumas' ? number1 + number2
+            : testKey == 'restas' ? number1 - number2
+            : testKey == 'multiplicaciones' ? number1 * number2
+            : number1 / number2,
+            valueToComplete
+        ];
+        const operationSymbol = testKey == 'sumas' ? '+'
+            : testKey == 'restas' ? '-'
+            : testKey == 'multiplicaciones' ? 'x'
+            : '/';
+        
+        test[testKey].push(numbers);
+
+        const excerciseElement = document.createElement('article'); 
+        excerciseElement.classList.add('excercise');
+        let content = `
+                <span class="excersiceNumber">${index + 1})</span>
+                <p>
+                    <span>${valueToComplete == 0 ? null : numbers[0]} ${operationSymbol}&nbsp</span>
+                    <span>${valueToComplete == 1 ? null : numbers[1]}</span>
+                    <span>&nbsp= ${valueToComplete == 2 ? null : numbers[2]}</span>
+                </p>
+        `; 
+        content = content.replace('null', '<input type="number"/>');
+        excerciseElement.innerHTML = content;
+
+        return excerciseElement;
+    }
+
+    const createExcercises = (key) => {
+        const nExcercises = parseInt(document.querySelector('input[name="nExcersices"]').value);
+        const excersices = document.getElementById(key).querySelector('.excersices-container');
+        excersices.innerHTML = '';
+        
+        for (let index = 0; index < nExcercises; index++) {
+            const sumas = getHtmlExcercise(index, key);
+            excersices.appendChild(sumas);
+        }
+    }
+
+    const showSections = () => {
+        const excersiceTypes = [
+            {
+                checkbox: document.querySelector('input[type="checkbox"][name="sumas"]'),
+                section:  document.getElementById('sumas')
+            },
+            {
+                checkbox: document.querySelector('input[type="checkbox"][name="restas"]'),
+                section:  document.getElementById('restas')
+            },
+            {
+                checkbox: document.querySelector('input[type="checkbox"][name="multiplicaciones"]'),
+                section:  document.getElementById('multiplicaciones')
+            },
+            {
+                checkbox: document.querySelector('input[type="checkbox"][name="divisiones"]'),
+                section:  document.getElementById('divisiones')
+            }
+        ];
+
+        
+        excersiceTypes.forEach(type => {
+            if (type.checkbox.checked) {
+                type.section.classList.remove('hide');
+                createExcercises(type.checkbox.name)
+            } else {
+                type.section.classList.add('hide');
+            }
+        })
+
+        document.querySelector('.calificarBtn').classList.remove('hide');
+    }
+
+    const generateTest = (event) => {
+        test = {
+            sumas: [],
+            restas: [],
+            multiplicaciones: [],
+            divisiones: []
+        };
+        event.preventDefault();
+        showSections();
+    }
+
+    const generateButton = document.getElementById('generateTest');
+    generateButton.addEventListener('click', generateTest);
+
+    // Get results
+    const getResults = () => {
+        const results = [];
+
+        Object.keys(test).forEach(key => {
+            if (test[key].length) {
+                const answers = document.getElementById(key).querySelectorAll('input[type="number"]');
+
+                for (let i = 0; i < answers.length; i++) {
+                    const answer = parseInt(answers[i].value);
+                    const indexToCompare = test[key][i][3];
+                    const value = test[key][i][indexToCompare];
+                    const isValid = answer === value;
+                    results.push(isValid);
+    
+                    if (isValid) {
+                        answers[i].parentNode.parentNode.parentNode.classList.remove('wrong');
+                        answers[i].parentNode.parentNode.parentNode.classList.add('good');
+                    
+                    } else {
+                        answers[i].parentNode.parentNode.parentNode.classList.add('wrong');
+                        answers[i].parentNode.parentNode.parentNode.classList.remove('good');
+                        
+                    }
+                }
+            }
+        });
+ 
+        const total = results.length;
+        const good = results.filter(result => result).length;
+        const wrong = results.filter(result => !result).length;
+        const nota = good * 100 / total
+   
+
+      var myWindow = window.open("calficacion","_blank" ,"width=300,height=200,top=200,left=600,toolbar=no,location=no,status=no,menubar=no");
+       myWindow.document.write(`Total: ${total} `);
+       myWindow.document.write(`<br> Correctas: ${good}`);
+       myWindow.document.write(`<br>  Incorrectas: ${wrong}`);
+       myWindow.document.write(`<br>  Nota: ${nota}`);
+       
+       alert(`
+            Total: ${total}
+            Correctas: ${good}
+            Incorrectas: ${wrong}
+            Nota: ${nota}
+            
+        `);
+    }
+ 
+    const getResultsButton = document.getElementById('calificarBtn');
+    getResultsButton.addEventListener('click', getResults);
 } 
